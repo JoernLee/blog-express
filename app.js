15905cc8beb1780290633481c0e25c5561a707b8
Joern <924901972@qq.com>
2021-02-20 20:36:26 +0800

    feat:新增数据库相关配置项和操作

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 // 引用我们写的路由
 const blogRouter = require('./routes/blog');
@@ -21,10 +21,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 // 注册我们自己的路由
 app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter);
