09dd0d5085ffc438e6043d589729e02357eedd35
Joern <924901972@qq.com>
2025-03-23 22:33:57 +0800

    feat: update user controller

--- controller/user.js
+++ controller/user.js
@@ -11,6 +11,15 @@ const login = (username, password) => {
     const sql = `
         select username, realname from users where username=${username} and password=${password}
     `;
+    // 防止sql注入
+    if (!username || !password) {
+        return Promise.resolve({});
+    }
+
+    // 检查用户名和密码格式
+    if (username.length < 3 || password.length < 6) {
+        return Promise.resolve({});
+    }
     // console.log('sql is', sql)
     return exec(sql).then(rows => {
         return rows[0] || {}
